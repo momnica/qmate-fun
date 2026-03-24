63fc77433bfc989f9c6bc9994d0a7ca9ebe8bc28
momnica <55236837+momnica@users.noreply.github.com>
2026-03-24 22:25:05 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from 'qmate-fun/src/App.jsx'
+import App from '../src/App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
